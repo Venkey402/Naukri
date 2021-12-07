ce0364cc254e7edfc6b99b4f2468f74256ef7533
venkey402 <venkey402@gmail.com>
2021-12-07 13:47:07 +0530

    Added extent reports

---
+++ src/test/java/com/Naukri31/Testcases/test.java
@@ -0,0 +1,23 @@
+package com.Naukri31.Testcases;
+
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test
+	public void method1()
+	{
+		int i = 1/0;
+	}
+	@Test
+	public void method2()
+	{
+		int i = 1/0;
+	}
+	@Test
+	public void method3()
+	{
+		int i = 1/0;
+	}
+
+}
